312699c98f1f8acee95e16a9d9354ec530bb6f20
clx1608@qq.com <hbdennischen@outlook.com>
2018-05-05 23:28:23 +0800

    使用分支自己的测试文件

--- tests/SalesDb.test.js
+++ tests/SalesDb.test.js
@@ -16,69 +16,70 @@ describe('Application', function () {
         stubs['@finelets/hyper-rest/app'] = {createErrorReason: createReasonStub};
     });
 
-    describe('销售数据库', function () {
-        var salesDb, dbModels;
-        before(function () {
-            dbModels = require('../server/modules/sales/db/DbModels');
-        });
-
-        beforeEach(function () {
-            salesDb = require('../server/modules/sales/db/SalesDb');
-        });
-
-        describe('新增订单', function () {
-            var orderData, order, dbSaverStub;
-            it('成功', function () {
-                orderData = {orderData: 'any data of order'};
-                order = {order: 'order data from db'};
-                dbSaverStub = sinon.stub();
-                dbSaverStub.withArgs(dbModels.SalesOrder, orderData).returns(Promise.resolve(order));
-                stubs['@finelets/hyper-rest/db/mongoDb/SaveObjectToDb'] = dbSaverStub;
-                salesDb = proxyquire('../server/modules/sales/db/SalesDb', stubs);
-                return salesDb.createOrder(orderData)
-                    .then(function (data) {
-                        expect(data).eqls(order);
-                    })
-            })
-        });
-
-        describe('列出未锁定的订单草稿', function () {
-            var orderStatus, order;
-            beforeEach(function (done) {
-                orderStatus = require('../server/modules/sales/db/models/SalesOrderStatus');
-                order = {productLine: 'foo', status: orderStatus.statusValues.DRAFT};
-                clearDB(done);
+    describe('销售子系统', function () {
+        describe('销售数据库', function () {
+            var salesDb, dbModels;
+            before(function () {
+                dbModels = require('../server/modules/sales/db/DbModels');
             });
 
-            it('无任何记录', function () {
-                return salesDb.listUnlockedDraftOrders()
-                    .then(function (data) {
-                        expect(data).eqls([]);
-                    })
+            beforeEach(function () {
+                salesDb = require('../server/modules/sales/db/SalesDb');
             });
 
-            it('排除已锁定等其他非订单草稿', function () {
-                order.status = order.status | orderStatus.statusValues.LOCKED;
-                return dbSave(dbModels.SalesOrder, order)
-                    .then(function () {
-                        return salesDb.listUnlockedDraftOrders(['status']);
-                    })
-                    .then(function (data) {
-                        expect(data).eqls([]);
-                    })
+            describe('新增订单', function () {
+                var orderData, order, dbSaverStub;
+                it('成功', function () {
+                    orderData = {orderData: 'any data of order'};
+                    order = {order: 'order data from db'};
+                    dbSaverStub = sinon.stub();
+                    dbSaverStub.withArgs(dbModels.SalesOrder, orderData).returns(Promise.resolve(order));
+                    stubs['@finelets/hyper-rest/db/mongoDb/SaveObjectToDb'] = dbSaverStub;
+                    salesDb = proxyquire('../server/modules/sales/db/SalesDb', stubs);
+                    return salesDb.createOrder(orderData)
+                        .then(function (data) {
+                            expect(data).eqls(order);
+                        })
+                })
             });
 
-            it('正常', function () {
-                return dbSave(dbModels.SalesOrder, order)
-                    .then(function (data) {
-                        order = {id: data.id, status: order.status};
-                        return salesDb.listUnlockedDraftOrders(['status']);
-                    })
-                    .then(function (data) {
-                        expect(data).eqls([order]);
-                    })
+            describe('列出未锁定的订单草稿', function () {
+                var orderStatus, order;
+                beforeEach(function (done) {
+                    orderStatus = require('../server/modules/sales/db/models/SalesOrderStatus');
+                    order = {productLine: 'foo', status: orderStatus.statusValues.DRAFT};
+                    clearDB(done);
+                });
+
+                it('无任何记录', function () {
+                    return salesDb.listUnlockedDraftOrders()
+                        .then(function (data) {
+                            expect(data).eqls([]);
+                        })
+                });
+
+                it('排除已锁定等其他非订单草稿', function () {
+                    order.status = order.status | orderStatus.statusValues.LOCKED;
+                    return dbSave(dbModels.SalesOrder, order)
+                        .then(function () {
+                            return salesDb.listUnlockedDraftOrders(['status']);
+                        })
+                        .then(function (data) {
+                            expect(data).eqls([]);
+                        })
+                });
+
+                it('正常', function () {
+                    return dbSave(dbModels.SalesOrder, order)
+                        .then(function (data) {
+                            order = {id: data.id, status: order.status};
+                            return salesDb.listUnlockedDraftOrders(['status']);
+                        })
+                        .then(function (data) {
+                            expect(data).eqls([order]);
+                        })
+                });
             });
         });
     });
-
 });
