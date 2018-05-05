266fca81aef357c59bda90353a19c43daaa68bff
clx1608@qq.com <hbdennischen@outlook.com>
2018-05-05 23:51:19 +0800

    Merge branch 'Sales' of https://github.com/hotax/Finelets into Sales
    # Conflicts:
    #	tests/clx.test.js

---
+++ tests/Ansteel.test.js
@@ -0,0 +1,18 @@
+const proxyquire = require('proxyquire'),
+    mongoose = require('mongoose'),
+    dbSave = require('@finelets/hyper-rest/db/mongoDb/SaveObjectToDb');
+
+describe('Application', function () {
+    var func, stubs, err, reason, createReasonStub;
+    before(function () {
+        mongoose.Promise = global.Promise;
+    });
+
+    beforeEach(function () {
+        stubs = {};
+        err = new Error('any error message');
+        reason = {reason: 'any reason representing any error'}
+        createReasonStub = sinon.stub();
+        stubs['@finelets/hyper-rest/app'] = {createErrorReason: createReasonStub};
+    });
+});
